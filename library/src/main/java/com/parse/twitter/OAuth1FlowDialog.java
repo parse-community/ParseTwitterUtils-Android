038cea2c5a2b143d354922ceb8193feb4507c3e4
John <jawnnypoo@gmail.com>
2019-03-05 01:29:39 -0600

    Migrate to AndroidX

--- library/src/main/java/com/parse/twitter/OAuth1FlowDialog.java
+++ library/src/main/java/com/parse/twitter/OAuth1FlowDialog.java
@@ -15,7 +15,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDialog;
+import androidx.appcompat.app.AppCompatDialog;
 import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
